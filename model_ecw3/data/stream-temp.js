d9f2b6abdc7c6accfa1aaab97ea4c82475da1d14
Justine Chukwuyem Obiazi <83650645+uncleboss12@users.noreply.github.com>
2024-03-01 08:42:33 -0800

    Add files via upload

---
+++ model_ecw3/data/stream-temp.js
@@ -0,0 +1,385 @@
+var data_json = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2020,
+                "Stream Temperature (AVG)": 29.09692396,
+                "Stream Temperature (MIN)": 26.55185702,
+                "Stream Temperature (MAX)": 31.65309694
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2021,
+                "Stream Temperature (AVG)": 29.14967707,
+                "Stream Temperature (MIN)": 26.59963615,
+                "Stream Temperature (MAX)": 31.69443091
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2022,
+                "Stream Temperature (AVG)": 29.20077172,
+                "Stream Temperature (MIN)": 26.6456149,
+                "Stream Temperature (MAX)": 31.73305268
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2023,
+                "Stream Temperature (AVG)": 29.25223587,
+                "Stream Temperature (MIN)": 26.69440398,
+                "Stream Temperature (MAX)": 31.77276291
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2024,
+                "Stream Temperature (AVG)": 29.30523996,
+                "Stream Temperature (MIN)": 26.74301592,
+                "Stream Temperature (MAX)": 31.81391231
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2025,
+                "Stream Temperature (AVG)": 29.35980999,
+                "Stream Temperature (MIN)": 26.79239345,
+                "Stream Temperature (MAX)": 31.85917366
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2026,
+                "Stream Temperature (AVG)": 29.41390995,
+                "Stream Temperature (MIN)": 26.84145409,
+                "Stream Temperature (MAX)": 31.90089624
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2027,
+                "Stream Temperature (AVG)": 29.46704518,
+                "Stream Temperature (MIN)": 26.88840086,
+                "Stream Temperature (MAX)": 31.94484052
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2028,
+                "Stream Temperature (AVG)": 29.52193682,
+                "Stream Temperature (MIN)": 26.93300395,
+                "Stream Temperature (MAX)": 31.99077554
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2029,
+                "Stream Temperature (AVG)": 29.57676584,
+                "Stream Temperature (MIN)": 26.97432533,
+                "Stream Temperature (MAX)": 32.04378909
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2030,
+                "Stream Temperature (AVG)": 29.63102359,
+                "Stream Temperature (MIN)": 27.01367927,
+                "Stream Temperature (MAX)": 32.09909005
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2031,
+                "Stream Temperature (AVG)": 29.68715338,
+                "Stream Temperature (MIN)": 27.05241198,
+                "Stream Temperature (MAX)": 32.15549918
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2032,
+                "Stream Temperature (AVG)": 29.74461577,
+                "Stream Temperature (MIN)": 27.08796902,
+                "Stream Temperature (MAX)": 32.21633851
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2033,
+                "Stream Temperature (AVG)": 29.80293258,
+                "Stream Temperature (MIN)": 27.11892478,
+                "Stream Temperature (MAX)": 32.27936835
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2034,
+                "Stream Temperature (AVG)": 29.86274659,
+                "Stream Temperature (MIN)": 27.14724044,
+                "Stream Temperature (MAX)": 32.35108067
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2035,
+                "Stream Temperature (AVG)": 29.92441309,
+                "Stream Temperature (MIN)": 27.1745093,
+                "Stream Temperature (MAX)": 32.42735181
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2036,
+                "Stream Temperature (AVG)": 29.98853335,
+                "Stream Temperature (MIN)": 27.20039946,
+                "Stream Temperature (MAX)": 32.51353532
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2037,
+                "Stream Temperature (AVG)": 30.05677052,
+                "Stream Temperature (MIN)": 27.23658659,
+                "Stream Temperature (MAX)": 32.60356388
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2038,
+                "Stream Temperature (AVG)": 30.12699495,
+                "Stream Temperature (MIN)": 27.27814869,
+                "Stream Temperature (MAX)": 32.6967739
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2039,
+                "Stream Temperature (AVG)": 30.19930933,
+                "Stream Temperature (MIN)": 27.31654983,
+                "Stream Temperature (MAX)": 32.79643876
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2040,
+                "Stream Temperature (AVG)": 30.27355587,
+                "Stream Temperature (MIN)": 27.35563455,
+                "Stream Temperature (MAX)": 32.90111206
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2041,
+                "Stream Temperature (AVG)": 30.42355587,
+                "Stream Temperature (MIN)": 27.39353477,
+                "Stream Temperature (MAX)": 33.00881769
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2042,
+                "Stream Temperature (AVG)": 30.57355587,
+                "Stream Temperature (MIN)": 27.4344678,
+                "Stream Temperature (MAX)": 33.12168046
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2043,
+                "Stream Temperature (AVG)": 30.72355587,
+                "Stream Temperature (MIN)": 27.47543032,
+                "Stream Temperature (MAX)": 33.24121562
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2044,
+                "Stream Temperature (AVG)": 30.87355587,
+                "Stream Temperature (MIN)": 27.5177931,
+                "Stream Temperature (MAX)": 33.35421091
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2045,
+                "Stream Temperature (AVG)": 31.02355587,
+                "Stream Temperature (MIN)": 27.55625214,
+                "Stream Temperature (MAX)": 33.46441112
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2046,
+                "Stream Temperature (AVG)": 31.17355587,
+                "Stream Temperature (MIN)": 27.59634884,
+                "Stream Temperature (MAX)": 33.5726874
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2047,
+                "Stream Temperature (AVG)": 31.32355587,
+                "Stream Temperature (MIN)": 27.63125662,
+                "Stream Temperature (MAX)": 33.68246075
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2048,
+                "Stream Temperature (AVG)": 31.47355587,
+                "Stream Temperature (MIN)": 27.66619811,
+                "Stream Temperature (MAX)": 33.79596785
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2049,
+                "Stream Temperature (AVG)": 31.62355587,
+                "Stream Temperature (MIN)": 27.71026941,
+                "Stream Temperature (MAX)": 33.9094363
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2050,
+                "Stream Temperature (AVG)": 31.77355587,
+                "Stream Temperature (MIN)": 27.75434071,
+                "Stream Temperature (MAX)": 34.02290475
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2051,
+                "Stream Temperature (AVG)": 31.92355587,
+                "Stream Temperature (MIN)": 27.79841201,
+                "Stream Temperature (MAX)": 34.1363732
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2052,
+                "Stream Temperature (AVG)": 32.07355587,
+                "Stream Temperature (MIN)": 27.84248331,
+                "Stream Temperature (MAX)": 34.24984165
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2053,
+                "Stream Temperature (AVG)": 32.22355587,
+                "Stream Temperature (MIN)": 27.88655461,
+                "Stream Temperature (MAX)": 34.3633101
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2054,
+                "Stream Temperature (AVG)": 32.37355587,
+                "Stream Temperature (MIN)": 27.93062591,
+                "Stream Temperature (MAX)": 34.47677855
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2055,
+                "Stream Temperature (AVG)": 32.52355587,
+                "Stream Temperature (MIN)": 27.97469721,
+                "Stream Temperature (MAX)": 34.590247
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2056,
+                "Stream Temperature (AVG)": 32.67355587,
+                "Stream Temperature (MIN)": 28.01876851,
+                "Stream Temperature (MAX)": 34.70371545
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2057,
+                "Stream Temperature (AVG)": 32.82355587,
+                "Stream Temperature (MIN)": 28.06283981,
+                "Stream Temperature (MAX)": 34.8171839
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2058,
+                "Stream Temperature (AVG)": 32.97355587,
+                "Stream Temperature (MIN)": 28.10691111,
+                "Stream Temperature (MAX)": 34.93065235
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2059,
+                "Stream Temperature (AVG)": 33.12355587,
+                "Stream Temperature (MIN)": 28.15098241,
+                "Stream Temperature (MAX)": 35.0441208
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2060,
+                "Stream Temperature (AVG)": 33.27355587,
+                "Stream Temperature (MIN)": 28.19505371,
+                "Stream Temperature (MAX)": 35.15758925
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "Year": 2061,
+                "Stream Temperature (AVG)": 33.42355587,
+                "Stream Temperature (MIN)": 28.23912501,
+                "Stream Temperature (MAX)": 35.2710577
+            }
+        }
+       
+    ]
+};
+
